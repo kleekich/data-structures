4ffa68fa6d78392d72cab1ad2f2c466ffd377b98
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-19 20:57:40 -0700

    Implements prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.i = 0;
+  obj.o = 0;
+  obj.storage = {};
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue: function(item) {
+		this.storage[this.i] = item;
+		this.i++;
+	},
+	dequeue: function() {
+		var dequeued = this.storage[this.o];
+		delete this.storage[this.o];
+		this.o++;
+		return dequeued;
+	},
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
