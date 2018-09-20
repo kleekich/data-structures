8af2f91d4751a1e3233d69d395feca8d9f769994
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-19 21:28:40 -0700

    Implements ES6 queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,23 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   constructor() {
+  	this.storage = {}
+  	this.i = 0;
+  	this.o = 0;
+  }
+
+  enqueue(item){
+  	this.storage[this.i++] = item;
+  }
+  dequeue(){
+  	var dequeued = this.storage[this.o];
+  	delete this.storage[this.o++];
+  	return dequeued;
+  }
+  size(){
+  	return Object.keys(this.storage).length;
   }
 
 }
